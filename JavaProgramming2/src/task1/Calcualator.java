49e9470c9d4b35eeda4b668bf2ad24c9affb2633
Yugeon-Kim <dbrjs0523@gmail.com>
2024-10-22 18:11:42 +0900

    Actionlistener 구현 성공

--- JavaProgramming2/src/task1/Calcualator.java
+++ JavaProgramming2/src/task1/Calcualator.java
@@ -16,16 +16,30 @@ public class Calcualator extends JFrame {
 
 	JButton[] jbutton = new JButton[25];
 	JTextField text;
-
+	
+	ActionListener litener1= e-> {
+		 
+		String currentText = text.getText();
+		String btn_s = ((JButton) e.getSource()).getText();
+		
+		 if (currentText.equals("0")) {
+			 text.setText(btn_s);
+		 }
+		 else {
+			 text.setText(currentText+btn_s);
+		 }
+	
+	 };
+	
 	public Calcualator() {
-
+		
 		text = new JTextField("0");
 		text.setEditable(false);
 		text.setFont(new Font("Dialog",Font.PLAIN,55));
 		
 		text.setHorizontalAlignment(JTextField.RIGHT); 
 		add(text, BorderLayout.NORTH);
-
+		
 		showButton();
 		setSize(340, 400);
 		this.setVisible(true);
@@ -38,22 +52,20 @@ public class Calcualator extends JFrame {
 			jp1.add(jbutton[i] = new JButton(button_text[i]));
 			jbutton[i].setBackground(Color.white);
 			jbutton[i].setFont(new Font("Noto Sans", Font.PLAIN, 17)); 
+			jbutton[i].addActionListener(litener1);
 		}
 		jbutton[23].setForeground(Color.white);
 		jbutton[23].setBackground(new Color(0,103,192));
 		add(jp1, BorderLayout.CENTER);
 		
-		 ActionListener litener1= e-> {
-				String s =text.getText();
-				String btn_s = jbutton[i].getText();
-				int z = Integer.parseInt(s);
-				result
+		 
 			}
 
-	}
+	
 
 	public static void main(String[] args) {
 		new Calcualator();
 	}
 
 }
+
